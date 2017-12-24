c8d7b344b827b98462d5009a20f44540796c1861
Lightning <lightning-zgc@hotmail.com>
2017-12-24 20:19:38 +0800

    bug fix

--- api/all.php
+++ api/all.php
@@ -1,7 +1,6 @@
 <?php
 $all = 0;
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://115.29.79.91:5000/get_err");
 curl_setopt($ch, CURLOPT_POST, 0);
 curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false); //  PHP 5.6.0 后必须开启
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -10,7 +9,6 @@ curl_close($ch);
 $all += len(json_decode($data));
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://120.79.76.18:5000/get_err");
 curl_setopt($ch, CURLOPT_POST, 0);
 curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false); //  PHP 5.6.0 后必须开启
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -18,7 +16,6 @@ $data = curl_exec($ch);
 $all += len(json_decode($data));
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "http://120.78.156.12:5000/get_err");
 curl_setopt($ch, CURLOPT_POST, 0);
 curl_setopt($ch, CURLOPT_SAFE_UPLOAD, false); //  PHP 5.6.0 后必须开启
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
